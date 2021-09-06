75ceb9b3d5d4a82ab02782eff4804c7b5308d646
Erik Shelton <erik.shelton328@gmail.com>
2021-09-06 12:33:37 -0500

    Created basic navbar with links to other pages. Imported navbar in home page

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  Navbar,
+  Nav,
+  Container
+} from 'react-bootstrap';
+
+const NavBar = () => {
+  return (
+    <Navbar expand='sm' bg='light'>
+      <Container>
+        <Navbar.Brand>
+          Logo
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+          <Nav className="justify-content-end">
+            <Nav.Item>
+              <Nav.Link href='/'>Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href='/about'>About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href='/services'>Services</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href='/functionality'>Functionality</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href='/contact'>Conctact</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavBar from '../components/NavBar.js';
 
 const Home = () => {
 	return (
 		<div>
+			<NavBar />
 			This is the home page.
 		</div>
 	);
