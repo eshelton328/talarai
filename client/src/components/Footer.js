8fdd89ab94002cc33ed6d92fb89324b4a7c75f2e
Erik Shelton <erik.shelton328@gmail.com>
2021-09-06 15:47:55 -0500

    Added content to footer as well as styled it with main color. Postioned it so it sticks to the bottom of the page.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+} from 'react-bootstrap';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='footer'>
+      <Container>
+        <Row>
+          <Col>
+            &copy; 2021 <a href='https://www.talarai.com/'>talarai.com</a>
+            <br/>
+            2101 S. IH35 Frontage Rd.
+            <br/>
+            Austin, TX. 78741
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
