e734205598ecf4a1b48f87117300e2b3a7fb2a59
Erik Shelton <erik.shelton328@gmail.com>
2021-09-06 11:43:42 -0500

    Imported pages in App.js and created paths using react-router

--- client/App.js
+++ client/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Home = React.lazy(() => import('./src/pages/Home.js'));
+const About = React.lazy(() => import('./src/pages/About.js'));
+const Services = React.lazy(() => import('./src/pages/Services.js'));
+const Functionality = React.lazy(() => import('./src/pages/Functionality.js'));
+const Contact = React.lazy(() => import('./src/pages/Contact.js'));
 
 const App = () => {
 	return (
-		<div>
-			Hello World!
-		</div>
+    <Router>
+      <Suspense fallback={<div></div>}>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/services' component={Services} />
+          <Route path='/functionality' component={Functionality} />
+          <Route path='/contact' component={Contact} />
+        </Switch>
+      </Suspense>
+    </Router>
 	);
 };
 
