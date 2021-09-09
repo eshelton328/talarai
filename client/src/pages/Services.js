430e1586d75f56785637d9c5c29eb0ed76580e84
Erik Shelton <erik.shelton328@gmail.com>
2021-09-09 13:52:07 -0500

    Transformed image on Services page to a card with a footer

--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -8,6 +8,7 @@ import {
 	Row,
 	Col,
 	Image,
+	Card,
 } from 'react-bootstrap'
 
 const Services = () => {
@@ -19,9 +20,19 @@ const Services = () => {
 				<Row xs={1} sm={1} md={1} lg={1} className='serv-row'>
 					<Col className='center'>Our staff has experience working in the following countries: South Africa, U.S., Oman, Qatar, Saudi Arabia, Egypt, Turkey, UAE, Iraq, Afghanistan, Curacao, Aruba, Ecuador, Uruguay, Argentina, United Kingdom, Germany, Netherlands, and more. Below is a sample of some of the Agencies, Clients, and Companies that our personnel has worked with:</Col>
 					<Col className='center serv-img'>
-						<Image src={Departments} fluid='true'/>
+						<Card style={{border: 'none'}}>
+							<Card.Img variant="top" src={Departments} fluid='true' />
+							<Card.Body>
+								<Card.Text style={{fontSize: '14px'}}>
+									Talarai, LLC was founded to provide quality, cost-effective, services to  Governmental clients.
+									<br/>
+									<b>A Certified Service Disabled Veteran Owned Business.</b>
+								</Card.Text>
+							</Card.Body>
+						</Card>
 					</Col>
-					<Col>Located in central Texas, Talarai offers a full line of customized solutions for government agencies in the information technology and management field, including AI, IT Consulting, Web Design and Development, Project Management Services, Software, and App Development Solutions Management, and more. We are continuously evolving to meet our Federal, State, and Local client’s needs and ensure our solutions remain current with the latest trends in the technology industry.
+					<Col>
+					Located in central Texas, Talarai offers a full line of customized solutions for government agencies in the information technology and management field, including AI, IT Consulting, Web Design and Development, Project Management Services, Software, and App Development Solutions Management, and more. We are continuously evolving to meet our Federal, State, and Local client’s needs and ensure our solutions remain current with the latest trends in the technology industry.
 					<br/><br/>
 					We can set up and maintain your business's network or help troubleshoot and maintain your existing system and provide professional services in all areas, from hardware to information technology software.
 					<br/><br/>
