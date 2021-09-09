d312d6b93e596f5333b0ccf3a98d0dc901b46702
Erik Shelton <erik.shelton328@gmail.com>
2021-09-08 20:24:27 -0500

    Created LargeCard component to clean up reusable code

---
+++ client/src/components/LargeCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  Row,
+  Col,
+  Image,
+} from 'react-bootstrap';
+
+const LargeCard = ({ info }) => {
+  return (
+    <Row xs={1} sm={1} md={1} lg={2} className='lcs-row'>
+      <Col className='lcs-col lcs-img center'>
+        <Image src={info.img} fluid='true' />
+      </Col>
+      <Col className='lcs-col lcs-text'>
+        <div>
+          <h1>{info.title}</h1>
+          <p>{info.data}</p>
+        </div>
+      </Col>
+    </Row>
+  );
+};
+
+export default LargeCard;
