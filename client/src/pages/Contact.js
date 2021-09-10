31a6518f88d2779f1fa6a56d7b2c7da1486ba2c4
Erik Shelton <erik.shelton328@gmail.com>
2021-09-10 14:11:14 -0500

    Added form validation. Updated state for values changed in the form

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -11,46 +11,124 @@ import {
 } from 'react-bootstrap';
 
 const Contact = () => {
+
+	const [validated, setValidated] = useState(false);
+	const [first, setFirst] = useState('');
+	const [last, setLast] = useState('');
+	const [userEmail, setUserEmail] = useState('');
+	const [sendTo, setSendTo] = useState('');
+	const [subject, setSubject] = useState('');
+	const [message, setMessage] = useState('');
+	const [newsLetter, setNewsLetter] = useState(false);
+
+	const handleSubmit = (event) => {
+		console.log('Validating...');
+		const form = event.currentTarget;
+		if (form.checkValidity() === false) {
+			event.preventDefault();
+			event.stopPropagation();
+			console.log(form);
+		} else {
+			console.log(first);
+			console.log(last);
+			console.log(userEmail);
+			console.log(sendTo);
+			console.log(subject);
+			console.log(newsLetter);
+		}
+
+		setValidated(true);
+	};
+
 	return (
 		<div>
 			<NavBar curr={'contact'} />
 			<PageTitle title={'Contact'} />
 			<Container className='contact-container'>
-				<Form>
+				<Form noValidate validated={validated} onSubmit={handleSubmit}>
 					<Row className="mb-3">
 						<Form.Group as={Col}>
 							<Form.Label>First Name</Form.Label>
-							<Form.Control type="text" placeholder="First" />
+							<Form.Control
+								required
+								type="text"
+								placeholder="First"
+								onChange={(e) => {
+									setFirst(e.target.value);
+								}}
+							/>
 						</Form.Group>
 						<Form.Group as={Col}>
 							<Form.Label>Last Name</Form.Label>
-							<Form.Control type="text" placeholder="Last" />
+							<Form.Control
+								required
+								type="text"
+								placeholder="Last"
+								onChange={(e) => {
+									setLast(e.target.value);
+								}}
+							/>
 						</Form.Group>
 						<Form.Group as={Col} controlId="formGridEmail">
 							<Form.Label>Your Email</Form.Label>
-							<Form.Control type="email" placeholder="email@example.com" />
+							<Form.Control
+								required
+								type="email"
+								placeholder="email@example.com"
+								onChange={(e) => {
+									setUserEmail(e.target.value);
+								}}
+							/>
 						</Form.Group>
 					</Row>
 					<Row>
 						<Form.Group as={Col} controlId="formGridEmail">
 							<Form.Label>Send Email To</Form.Label>
-							<Form.Control type="email" placeholder="email@example.com" />
+							<Form.Control
+								required
+								type="email"
+								placeholder="email@example.com"
+								onChange={(e) => {
+									setSendTo(e.target.value);
+								}}
+							/>
 						</Form.Group>
 						<Form.Group as={Col}>
 							<Form.Label>Subject</Form.Label>
-							<Form.Control type="text" placeholder="Subject" />
+							<Form.Control
+								required
+								type="text"
+								placeholder="Subject"
+								onChange={(e) => {
+									setSubject(e.target.value);
+								}}
+							/>
 						</Form.Group>
 					</Row>
 					<Row>
 						<Form.Group as={Col} controlId="exampleForm.ControlTextarea1">
 							<Form.Label></Form.Label>
-							<Form.Control as="textarea" rows={8} placeholder='Your message' />
+							<Form.Control
+								required
+								as="textarea"
+								rows={8}
+								placeholder='Your message'
+								onChange={(e) => {
+									setMessage(e.target.value);
+								}}
+							/>
 						</Form.Group>
 					</Row>
 					<Row>
 					</Row>
 					<Form.Group className='news-letter' id="formGridCheckbox">
-						<Form.Check type="checkbox" label="I want to recieve newsletters" />
+						<Form.Check
+							type="checkbox"
+							label="I want to recieve newsletters"
+							onClick={() => {
+								setNewsLetter(!newsLetter);
+							}}
+							/>
 					</Form.Group>
 					<Button type="submit">Send</Button>
 				</Form>
