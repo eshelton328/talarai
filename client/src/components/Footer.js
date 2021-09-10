a360107ed32d554dfd2bb5fda7b9e87f392e99c8
Erik Shelton <erik.shelton328@gmail.com>
2021-09-10 17:39:40 -0500

    Added image to Footer and aligned text and images closer to margins of page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import SDVOSB from '../assets/img/sdvosb.jpeg'
 import {
   Container,
   Row,
   Col,
+  Image,
 } from 'react-bootstrap';
 
 const Footer = () => {
   return (
     <div className='footer'>
-      <Container>
+      <Container fluid='true'>
         <Row>
-          <Col>
+          <Col className='footer-text'>
             &copy; 2021 <a href='https://www.talarai.com/'>talarai.com</a>
             <br/>
             2101 S. IH35 Frontage Rd.
             <br/>
             Austin, TX. 78741
+          </Col>
+          <Col className='footer-img'>
+            <Image src={SDVOSB} style={{width: "5rem"}} roundedCircle='true' />
           </Col>
         </Row>
       </Container>
