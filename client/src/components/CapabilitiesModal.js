fd98fdab20437455da38ea01fab278bed344096c
Erik Shelton <erik.shelton328@gmail.com>
2021-09-11 12:11:51 -0500

    Added Modal component. When services button is clicked the modal is toggled with capabilities image

---
+++ client/src/components/CapabilitiesModal.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Capabilities from '../assets/img/capabilities.jpeg'
+import {
+  Modal,
+  Button,
+  Image,
+} from 'react-bootstrap';
+
+const CapabilitiesModal = (props) => {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton />
+      <Modal.Body className='center'>
+        <Image src={Capabilities} fluid='true'/>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default CapabilitiesModal;
